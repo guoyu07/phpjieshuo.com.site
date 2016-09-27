ffd937e326d2b636150357f24bf992b690a9ad68
winerQin <753814253@qq.com>
2016-09-27 23:44:12 +0800

    add

--- themes/navy/source/js/lang_select.js
+++ themes/navy/source/js/lang_select.js
@@ -5,7 +5,7 @@
     var lang = this.value;
     var canonical = this.dataset.canonical;
     if (lang === 'en') lang = '';
-    if (lang) lang += '/';
+    if (lang) lang = '';
 
     location.href = '/' + lang + canonical;
   }
